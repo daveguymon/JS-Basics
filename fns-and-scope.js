49785e18403b93da66b8cb3a8262908cd4b0bd6d
Dave Guymon <davidwguymon@gmail.com>
2017-06-23 14:51:29 -0600

    Problem solutions

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -4,7 +4,13 @@ var name = 'Tyler';
 //Create a function called isTyler that accepts name as it's only parameter.
 //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
 
-  //Code Here
+function isTyler(name){
+  if(name==="Tyler"){
+    return true;
+  } else {
+    return false;
+  }
+}
 
 //////////////////PROBLEM 2////////////////////
 
@@ -12,7 +18,11 @@ var name = 'Tyler';
 //Create a function called getName that uses prompt() to prompt the user for their name, then returns the name.
 
 
-  //Code Here
+function getName(){
+  var userName = prompt("What is your name?");
+  return userName;
+  
+}
 
 
 //////////////////PROBLEM 3////////////////////
@@ -22,7 +32,10 @@ var name = 'Tyler';
 //Create a function called welcome that uses your getName function you created in the previous problem to get the users name,
 //then alerts "Welcome, " plus whatever the users name is.
 
-  //Code Here
+ function welcome(){
+   var response = getName();
+   alert("Welcome, " + response);
+ }
 
 
 //////////////////PROBLEM 4////////////////////
@@ -32,7 +45,7 @@ var name = 'Tyler';
 
 //What is the difference between arguments and parameters?
 
-  //Answer Here
+  // Parameters are variable placeholders that are part of the function's shell. Arguments are types received by the function when it is invoked. Arguments are assigned to their corresponding parameter within the function and used by the function to create an output.
 
 
 //////////////////PROBLEM 5////////////////////
@@ -42,7 +55,21 @@ var name = 'Tyler';
 //What are all the falsy values in JavaScript and how do you check if something is falsy?
 
 
-  //Answer Here
+  // falsy values include 0, '', false, null, undefined, & NaN
+
+  // To check if something is falsy, you can use a function like the one below:
+
+// var val='';
+
+//   function isFalsy(a){
+//     if(!a){
+//       return 'Falsy';
+//     } else {
+//       return 'Truthy';
+//     }
+//   }
+
+//   isFalsy(val);
 
 
 
@@ -52,16 +79,21 @@ var name = 'Tyler';
 
 //Create a function called myName that returns your name
 
-  //Code Here
+  function myName(){
+    var name = prompt('What is your name?');
+    return name;
+  }
+
+  
   
 
 
 //Now save the function definition of myName into a new variable called newMyName
 
-  //Code Here
+  var newMyName = myName;
 
 //Now alert the result of invoking newMyName
-
+alert(newMyName);
 
 
 //////////////////PROBLEM 7////////////////////
@@ -70,10 +102,17 @@ var name = 'Tyler';
 
 //Create a function called outerFn which returns an anonymous function which returns your name.
 
-  //Code Here
+  function outerFn(){
+    return function(){
+      var myName = prompt('What is your name?');
+      return myName;
+    }
+  }
 
 //Now save the result of invoking outerFn into a variable called innerFn.
 
   //Code Here
+  var innerFn = outerFn();
 
 //Now invoke innerFn.
+innerFn;
